49d7e4ad011b74ade265787cd4db71ac51094de9
Jarrett <khunicycler@hotmail.com>
2018-06-07 16:25:03 -0600

    countdown: initialize models for db

--- countdown/models.py
+++ countdown/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Semester(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Day(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE, blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    date = models.DateField()
