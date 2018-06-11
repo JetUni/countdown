55adbfa06ecec4dcafe12948e23f1b9e6c8c7271
Jarrett <khunicycler@hotmail.com>
2018-06-10 18:25:45 -0600

    countdown: admin: setup for models

--- countdown/admin.py
+++ countdown/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = ('date', 'category', 'semester', 'year')
+    list_filter = ('category', 'semester', 'year')
+
+
+admin.site.register(Category)
+admin.site.register(Semester)
+admin.site.register(Day, DayAdmin)
