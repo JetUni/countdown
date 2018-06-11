f773b9f3d0719af7ec0a7f469e621da912a1ff9d
Jarrett <khunicycler@hotmail.com>
2018-06-11 10:01:41 -0600

    countdown: create view for base countdown

--- countdown/views.py
+++ countdown/views.py
@@ -1,3 +1,11 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+
+def index(request):
+    context = {}
+    context['total_days'] = Day.objects.filter(category_id=1, year=datetime.now().year).count()
+    context['days_left'] = Day.objects.filter(category_id=1, date__gt=datetime.now()).count()
+    return render(request, 'countdown/countdown.html', context)
