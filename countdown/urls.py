9bf627a9189235ee9e4172d27aa5ebcf5cd3c118
Jarrett <khunicycler@hotmail.com>
2018-06-12 12:03:09 -0600

    countdown: rework to support baby countdown

--- countdown/urls.py
+++ countdown/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:category_id>/', views.index, name='count_by_category'),
 ]
--- countdown/views.py
+++ countdown/views.py
@@ -1,11 +1,27 @@
 from datetime import datetime, timedelta
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import *
 
 
-def index(request):
+def index(request, category_id=1):
     context = {}
-    context['total_days'] = Day.objects.filter(category_id=1, year=datetime.now().year).count()
-    context['days_left'] = Day.objects.filter(category_id=1, date__gt=datetime.now()).count()
+    category = get_object_or_404(Category, pk=category_id)
+    today = datetime.now().date()
+    if category.name == "School":
+        for s in Semester.objects.all():
+            if s.start <= today and s.end >= today:
+                semester = s
+        total_days = Day.objects.filter(category_id=category_id, semester=semester, year=datetime.now().year).count()
+        days_left = Day.objects.filter(category_id=category_id, semester=semester, year=datetime.now().year, date__gt=datetime.now()).count()
+        context['title'] = "{} {}".format(semester, datetime.now().year)
+    else:
+        total_days = Day.objects.filter(category_id=category_id).count()
+        days_left = Day.objects.filter(category_id=category_id, date__gt=datetime.now()).count()
+        context['title'] = category.name
+
+    context['category'] = category
+    context['total_days'] = total_days
+    context['days_left'] = days_left
+
     return render(request, 'countdown/countdown.html', context)
