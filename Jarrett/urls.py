94ceeb6af020bd71e9f46163a774c52e64347e7a
Jarrett <khunicycler@hotmail.com>
2018-06-12 11:55:08 -0600

    base: enable index

--- Jarrett/urls.py
+++ Jarrett/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from . import views
+
 
 urlpatterns = [
     path('countdown/', include('countdown.urls')),
     path('admin/', admin.site.urls),
+    path('', views.index, name='index')
 ]
