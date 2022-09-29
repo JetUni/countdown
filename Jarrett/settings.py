cf3652ea9801559022f322d2f5627e96c4743509
Jarrett Sorensen <khunicycler@hotmail.com>
2022-09-29 15:29:52 -0600

    settings: protect SECRET_KEY
    This was never a problem until I made this repo public. Now the key
    has been exposed, so I've hidden it in the local_settings file and
    generated a new one

--- Jarrett/settings.py
+++ Jarrett/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yu#^k+&^egkwds574ogzjd65lt5=qr(htnucxed9&*jspnvsgo'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = local_settings.DEBUG
